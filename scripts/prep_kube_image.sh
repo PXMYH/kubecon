00a15edc7db83d1b50d03d6b2742b127ca174c41
Michael Hu <pxmyhdev@gmail.com>
2018-04-04 21:33:34 -0400

    scp file to host

--- scripts/prep_kube_image.sh
+++ scripts/prep_kube_image.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+# connect to host
+echo $HOST_PRIVATE_KEY > host_priv.pem
+chmod 400 host_priv.pem
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+cat << EOF > prep.sh
+  sudo apt-get remove docker docker-engine docker.io
+  sudo apt-get update
+  sudo apt-get install \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      software-properties-common
 
-sudo apt-get update
-sudo apt-get install docker-ce
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
-sudo systemctl disable docker
+  sudo apt-get update
+  sudo apt-get install docker-ce
+
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  sudo systemctl enable docker
+  sudo systemctl disable docker
+EOF
+
+cat prep.sh
+
+scp prep.sh -i host_priv.pem ubuntu@$HOST_IP:~
