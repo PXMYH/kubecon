4ff05d254a70ec0523d3a2bf34bc4626b553927a
Michael Hu <pxmyhdev@gmail.com>
2018-04-04 22:18:23 -0400

    fix deployment script

--- scripts/deploy_kube_image.sh
+++ scripts/deploy_kube_image.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add 
+# connect to host
+echo "$HOST_PRIVATE_KEY" > host_priv.pem
+chmod 400 host_priv.pem
 
-deb http://apt.kubernetes.io/ kubernetes-xenial main 
+cat << EOF > install_kube.sh
+  sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add 
+  deb http://apt.kubernetes.io/ kubernetes-xenial main 
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+  apt-get update -y
+  apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+EOF
+
+chmod +x install_kube.sh
+
+echo "install_kube.sh content:"
+cat install_kube.sh
+
+scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i host_priv.pem install_kube.sh ubuntu@$HOST_IP:~
+ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i host_priv.pem ubuntu@$HOST_IP 'bash -s' < install_kube.sh
