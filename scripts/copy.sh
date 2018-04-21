5913bcc477e2eecbd7ac052ba8eff36e1119d6c1
Michael Hu <pxmyhdev@gmail.com>
2018-04-21 09:21:06 -0400

    test to see why fly is missing

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -4,6 +4,8 @@ set -xeu
 
 cp -r deploy-repo/. builddir/
 cp jq-github-release/jq-linux64 builddir/jq
+ls concourse-github-release
+wget https://github.com/concourse/concourse/releases/download/v3.11.0/fly_linux_amd64 -O concourse-github-release/fly_linux_amd64
 cp concourse-github-release/fly_linux_amd64 builddir/fly
 cp cf-cli-s3-release/cf builddir/cf
 cp bosh-s3-release/bosh-cli-*-linux-amd64 builddir/bosh
