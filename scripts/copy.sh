124113340c427e2ee48d6a7d40176989c401e314
Michael Hu <pxmyhdev@gmail.com>
2018-06-18 21:22:27 -0400

    fix missing input

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -9,6 +9,8 @@ fly_download_url="https://github.com/concourse/concourse/releases/download/"
 wget ${fly_download_url}/${fly_version}/fly_linux_amd64 -O concourse-github-release/fly_linux_amd64
 cp concourse-github-release/fly_linux_amd64 builddir/fly
 cp cf-cli-s3-release/cf builddir/cf
+ls -al
+ls -al spruce-github-release
 cp spruce-github-release/spruce-linux-amd64 builddir/spruce
 cp bosh-s3-release/bosh-cli-*-linux-amd64 builddir/bosh
 cp yaml-github-release/yq_linux_amd64 builddir/yaml
